9dd1799931bf36758a589433feb9449ccb283934
eymeric <eymericdechelette@gmail.com>
2023-12-01 10:12:37 +0100

    init express with ts

---
+++ back/flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    # You can override nixpkgs to use the latest set of node packages
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {
+    systems,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (
+        system:
+          f nixpkgs.legacyPackages.${system}
+      );
+  in {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nodejs
+          # You can set the major version of Node.js to a specific one instead
+          # of the default version
+          # pkgs.nodejs-19_x
+
+          # You can choose pnpm, yarn, or none (npm).
+          #nodePackages.pnpm
+          # pkgs.yarn
+
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+          node2nix
+        ];
+      };
+    });
+  };
+}
