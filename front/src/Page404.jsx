b8f49fb41c036bf4dfd5f331db88e8689696b939
Claquettes <math@claq.fr>
2023-12-07 20:08:13 +0100

    place one of the tetris pieces in the top of the grid

--- front/src/Page404.jsx
+++ front/src/Page404.jsx
@@ -1,33 +1,85 @@
-import React, { useEffect, useState } from 'react';
-import './page404.scss'
-import './App.scss'
+import React, { useEffect, useState } from "react";
+import "./page404.scss";
+import "./App.scss";
 
 function Page404() {
-  const [cells, setCells] = useState([]);
+  const [grid, setGrid] = useState([]);
 
-  useEffect(() => {
-    const newCells = [];
-    for (let i = 0; i < 300; i++) {
-      newCells.push(<div className='cell' key={i}></div>);
+  //we create a 10 x 7 grid
+  const createGrid = () => {
+    const grid = [];
+    for (let i = 0; i < 20; i++) {
+      grid.push(new Array(10).fill(9));
     }
-    setCells(newCells);
-  }, []);
+    return grid;
+  };
 
-  //we randomly select a cell and change this class to "block"
+  const pieces = {
+    0: [
+      [1, 1],
+      [1, 1],
+    ],
+    1: [[1, 1, 1, 1]],
+    2: [
+      [1, 1, 0],
+      [0, 1, 1],
+    ],
+    3: [
+      [0, 1, 1],
+      [1, 1, 0],
+    ],
+    4: [
+      [1, 0, 0],
+      [1, 1, 1],
+    ],
+    5: [
+      [0, 0, 1],
+      [1, 1, 1],
+    ],
+    6: [
+      [0, 1, 0],
+      [1, 1, 1],
+    ],
+  };
+
+  const generateRandomPiece = () => {
+    const randomPiece = Math.floor(Math.random() * 7);
+    return pieces[randomPiece];
+  };
+
+
+  const placePiece = (piece, grid) => {
+    const newGrid = grid.map((row) => [...row]);
+   //we are careful to place the piece in the middle of the grid
+    const middle = Math.floor(newGrid[0].length / 2);
+    for (let i = 0; i < piece.length; i++) {
+      for (let j = 0; j < piece[i].length; j++) {
+        newGrid[i][middle + j] = piece[i][j];
+      }
+    }
+    return newGrid;
+  }
+
+//start of the game
   useEffect(() => {
-    const interval = setInterval(() => {
-      const newCells = [...cells];
-      const randomCell = Math.floor(Math.random() * 300);
-      newCells[randomCell] = <div className='cell block' key={randomCell}></div>;
-      setCells(newCells);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [cells]);
+    const grid = createGrid();
+    const piece = generateRandomPiece();
+    const newGrid = placePiece(piece, grid);
+    setGrid(newGrid);
+  }, []);
+  
 
   return (
-    <div className='tetris'>
-      <div id='grid'>
-        {cells}
+    <div className="tetris">
+      <div id="grid">
+        {grid.map((row, i) =>
+          row.map((cell, j) => (
+            <div
+              className={cell === 9 ? "cell" : "block"}
+              key={`${i}${j}`}
+            ></div>
+          ))
+        )}
       </div>
     </div>
   );
