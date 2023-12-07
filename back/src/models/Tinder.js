12c362b10dc501e0e6907bef58f1db3ae636b0b3
Timothé Longuet <longuet.timoth@yahoo.com>
2023-12-08 00:45:52 +0100

    API REST sans donnée dans la bdd

--- back/src/models/Tinder.js
+++ back/src/models/Tinder.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const TinderSchema = mongoose.Schema({
+    ImageId: { type: Number, required: true },
     Categorie: { type: String, required: true },
     DescriptionCategorie: { type: String, required: true },
     imageUrl: { type: String, required: true },
