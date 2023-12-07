a9c1b0eb2eb9997112298f1ffe0a6429c6f0927f
Eclairsombre <alexandre.thny@gmail.com>
2023-12-07 22:24:48 +0100

    working minimal tierlist

---
+++ front/src/TierList.jsx
@@ -0,0 +1,117 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import './TierList.scss';
+
+function TierList() {
+  const [stockage, setStockage] = useState([]);
+  const [S, setS] = useState([]);
+  const [A, setA] = useState([]);
+  const [B, setB] = useState([]);
+  const [C, setC] = useState([]);
+  const [D, setD] = useState([]);
+
+  class Card {
+    constructor(name, image) {
+      this.name = name;
+      this.image = image;
+    }
+  }
+
+  useEffect(() => {
+    const newStockage = [];
+    for (let i = 0; i < 10; i++) {
+      let card = new Card("card " + i, "https://picsum.photos/200");
+      newStockage.push(card);
+    }
+    setStockage(newStockage);
+  }, []);
+
+  function handleDrop(e, tier) {
+    e.preventDefault();
+    const card = stockage[0];
+
+    switch (tier) {
+      case 'S':
+        setS((S) => [...S, card]);
+        break;
+      case 'A':
+        setA((A) => [...A, card]);
+        break;
+      case 'B':
+        setB((B) => [...B, card]);
+        break;
+      case 'C':
+        setC((C) => [...C, card]);
+        break;
+      case 'D':
+        setD((D) => [...D, card]);
+        break;
+      default:
+        break;
+    }
+
+    setStockage(stockage.slice(1));
+  }
+
+  function handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <div>
+      <h1>TierList</h1>
+      <div className="TierList">
+        <div className="S" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'S')}>
+          <h2>S</h2>
+          {S.map((card) => (
+            <div className="Card" key={card.name}>
+              <img src={card.image} alt={card.name} />
+            </div>
+          ))}
+        </div>
+        <div className="A" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'A')}>
+          <h2>A</h2>
+          {A.map((card) => (
+            <div className="Card" key={card.name}>
+              <img src={card.image} alt={card.name} />
+            </div>
+          ))}
+        </div>
+        <div className="B" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'B')}>
+          <h2>B</h2>
+          {B.map((card) => (
+            <div className="Card" key={card.name}>
+              <img src={card.image} alt={card.name} />
+            </div>
+          ))}
+        </div>
+        <div className="C" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'C')}>
+          <h2>C</h2>
+          {C.map((card) => (
+            <div className="Card" key={card.name}>
+              <img src={card.image} alt={card.name} />
+            </div>
+          ))}
+        </div>
+        <div className="D" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'D')}>
+          <h2>D</h2>
+          {D.map((card) => (
+            <div className="Card" key={card.name}>
+              <img src={card.image} alt={card.name} />
+            </div>
+          ))}
+        </div>
+
+        <div className="StockageCard" onDragOver={handleDragOver} onDrop={(e) => handleDrop(e, 'Stockage')}>
+          {stockage.map((card) => (
+            <div className="Card" key={card.name}>
+              <img src={card.image} alt={card.name} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TierList;
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client'
 import AppRouter from './Router.jsx'
 import './index.scss'
 
+import TierList from './TierList.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppRouter />
+    <TierList />
   </React.StrictMode>,
 )
