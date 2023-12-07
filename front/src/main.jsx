f002c0dfa76474bcfce4b860e903150a7aa5c52b
Xeon0X <xeon0x.gaming@gmail.com>
2023-12-08 00:53:19 +0100

    Test

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -9,28 +9,50 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
+// ThreeJS
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+// Renderer
 const canvas = document.querySelector('canvas.webgl')
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, 700 / 700, 0.1, 1000 );
-
-const geo = new THREE.BoxGeometry(1, 1, 1);
-const geoS = new THREE.SphereGeometry(2, 30);
-const material = new THREE.MeshBasicMaterial({color: 0xfff000, wireframe: true});
-const mesh = new THREE.Mesh(geo, material)
-const meshS = new THREE.Mesh(geoS, material)
-
-camera.position.z = 3;
-meshS.position.x = 2;
-
-scene.add(mesh);
-scene.add(meshS);
-
 const renderer = new THREE.WebGL1Renderer({
     canvas: canvas
 })
-renderer.setSize(700, 700);
+renderer.setSize(width, height);
+renderer.setPixelRatio(window.devicePixelRatio);
 
-renderer.render(scene, camera)
+//Camera
+const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+camera.position.setZ(10);
+
+//Scene
+const scene = new THREE.Scene();
+
+//Light
+const light = new THREE.PointLight(0xffffff, 10);
+light.position.set(3, 3, 3);
+scene.add(light);
+
+//Mesh 2
+const geometry2 = new THREE.SphereGeometry(1, 20);
+const material2 = new THREE.MeshBasicMaterial({color: 0xfff000, wireframe: true});
+const sphere = new THREE.Mesh(geometry2, material2)
+sphere.position.x = 0;
+scene.add(sphere);
+
+//Orbit 2
+const moonOrbit = new THREE.Group();
+moonOrbit.add(sphere);
+scene.add(moonOrbit);
+sphere.position.set(0, 0, 8);
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+animate();
