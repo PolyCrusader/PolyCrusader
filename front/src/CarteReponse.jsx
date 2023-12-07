f403bfd538f0eb088b99b50ca449793a08051971
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-12-07 23:25:22 +0100

    css properties (colors) for CarteReponse & TinderCard

--- front/src/CarteReponse.jsx
+++ front/src/CarteReponse.jsx
@@ -11,6 +11,7 @@ function CarteReponse ({ side, onNextMatch }) {
 
     let content =" Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget sagittis quam. Nam in lacus ut massa rutrum ultricies. Suspendisse porta pulvinar dolor eget porta. Mauris egestas semper urna, sit amet convallis elit blandit aliquet. Donec facilisis, sapien sed sollicitudin feugiat, leo tortor egestas erat, vestibulum imperdiet metus odio et ipsum. Sed dolor sem, mattis vehicula augue id, blandit suscipit tellus. Integer fermentum at mi id luctus. Nullam aliquet porta quam a bibendum. Sed dapibus elementum posuere. Pellentesque euismod scelerisque eros gravida dictum.";
     let logo = viteLogo;
+    let isInfoTrue = true;
 
     useEffect(() => {
         if (side === "left") {
@@ -24,7 +25,7 @@ function CarteReponse ({ side, onNextMatch }) {
   return (
     <>
     <h1> {isCorrect ? "Vous avez donné la bonne réponse" : "Vous avez donné la mauvaise réponse"} </h1>
-    <div className="case-reponse" >
+    <div className="case-reponse" style={{backgroundColor: isInfoTrue ? "rgba(0,255,0,0.1)" : "rgba(255,0,0,0.1)"}}>
         <img src={logo} className="logo" alt="Vite logo" />
         <p className = "text-reponse">{content}</p>
     </div>
