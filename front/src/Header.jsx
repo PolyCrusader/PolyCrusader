ed9327b6388eccd019d9b7d4b954c39e32b8c7da
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-12-07 22:59:58 +0100

    added tinder page to router

---
+++ front/src/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+
+function Header() {
+    return (
+        <div>
+            <Link to="/">Home</Link>
+            <Link to="/page/2">Page 2</Link>
+            <Link to="/tinder">Tinder</Link>
+        </div>
+    )
+}
+
+export default Header;
