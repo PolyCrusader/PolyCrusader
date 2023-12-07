e92bde2cdc310612cb9cdf44b8dcdbba975d8697
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-12-07 21:18:01 +0100

    basic css display

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,27 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.scss";
 import LandingPageHome from "./LandingPageHome";
-import HeaderHome from "./HeaderHome.jsx";
 import Section2 from "./Section2.jsx";
 import Section3 from "./Section3.jsx";
 import Footer from "./Footer.jsx";
+import './App.scss'
+import TinderCard from './TinderCard'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <LandingPageHome></LandingPageHome>
-        <Section2></Section2>
-        <Section3></Section3>
-        <Footer></Footer>
-      </div>
-
-    </>
-  );
+    return (
+        <>
+            <div>
+                <LandingPageHome></LandingPageHome>
+                <Section2></Section2>
+                <Section3></Section3>
+                <Footer></Footer>
+                <TinderCard/>
+            </div>
+        </>
+    );
 }
 
-export default App;
+export default App
---
+++ front/src/TinderCard.jsx
@@ -0,0 +1,11 @@
+const TinderCard = () => {
+  return (
+    <>
+        <div className="left-div"></div>
+        <div className="right-div"></div>
+        <div className="tinder-card"></div>
+    </>
+  )
+}
+
+export default TinderCard
