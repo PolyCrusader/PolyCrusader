82b5f894aec1176d057dde2439cf0127820b3b99
Claquettes <math@claq.fr>
2023-12-07 09:26:10 +0100

    added a page 2 to the router

--- front/src/Router.jsx
+++ front/src/Router.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Page2 from "./Page2";
 
 
 export default function AppRouter() {
@@ -7,6 +8,7 @@ export default function AppRouter() {
         <Router>
         <Routes>
           <Route path="/" element={<App />} />
+          <Route path="/page/2" element={<Page2 />} />
           <Route
             path="*"
             element={
