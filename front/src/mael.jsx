b190a82ae25faea4cb5bfe2e2b821435154b9b16
Xeon0X <xeon0x.gaming@gmail.com>
2023-12-08 02:22:46 +0100

    Orbit rotation working

--- front/src/mael.jsx
+++ front/src/mael.jsx
@@ -5,93 +5,86 @@ import { useEffect } from 'react'
 import './index.scss'
 
 
-// ThreeJS
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-
-function Mael(){
-
-useEffect(() => {
+function Mael() {
+  useEffect(() => {
     const width = window.innerWidth;
     const height = window.innerHeight;
-    
+
     // Renderer
-    const canvas = document.querySelector('canvas.webgl')
+    const canvas = document.querySelector('canvas.webgl');
     const renderer = new THREE.WebGL1Renderer({
-        canvas: canvas
-    })
+      canvas: canvas,
+    });
     renderer.setSize(width, height);
     renderer.setPixelRatio(window.devicePixelRatio);
-    
-    //Camera
-    const camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+
+    // Camera
+    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
     camera.position.setZ(10);
-    
-    //Scene
+
+    // Scene
     const scene = new THREE.Scene();
-    
-    //Light
-    const light = new THREE.PointLight(0xffffff, 10);
-    light.position.set(3, 3, 3);
+
+    // Light
+    const light = new THREE.PointLight(0xffffff, 1000);
+    light.position.set(-7, 7, 7);
     scene.add(light);
-    
-    //Mesh 2
-    const geometry2 = new THREE.SphereGeometry(1, 20);
-    const material2 = new THREE.MeshBasicMaterial({color: 0xfff0f0, wireframe: true});
-    const sphere = new THREE.Mesh(geometry2, material2)
-    sphere.position.x = 0;
-    scene.add(sphere);
-    
-    //Import models
+
+    // Import models
     let earthModel;
     const loader = new GLTFLoader();
 
-    loader.load( '../public/the_earth.gltf', function ( gltf ) {
-        earthModel = gltf.scene;
-        earthModel.scale.set(1,1,1);
-        scene.add(gltf.scene);
-    }, undefined, function ( error ) {
-        console.error( error );
-    } );
+    loader.load('../public/the_earth.gltf', function (gltf) {
+      earthModel = gltf.scene;
+      earthModel.scale.set(1, 1, 1);
+      scene.add(earthModel);
 
-    //Orbit 2
-    const moonOrbit = new THREE.Group();
-    moonOrbit.add(sphere);
-    scene.add(moonOrbit);
-    sphere.position.set(0, 0, 8);
+      // Call animate here to start animation after the model is loaded
+      animate();
+    }, undefined, function (error) {
+      console.error(error);
+    });
+
+    // Orbit
+    const orbit = new THREE.Group();
+    orbit.add(camera);
+    scene.add(orbit);
+    camera.position.set(0, 0, 8);
+    camera.lookAt(0, 0, 0);
 
     const target = new THREE.Vector2();
     const windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
     document.addEventListener('mousemove', onMouseMove, false);
     const mouse = new THREE.Vector2();
-    
+
     function onMouseMove(event) {
-        mouse.x = (event.clientX - windowHalf.x)
-        mouse.y = (event.clientY - windowHalf.y)
+      mouse.x = event.clientX - windowHalf.x;
+      mouse.y = event.clientY - windowHalf.y;
     }
-    
+
     function animate() {
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
-      sphere.rotation.x += 0.01;
+      if (earthModel) {
+        earthModel.rotation.y += 0.01;
+      }
 
-      target.x = (1 - mouse.x) * 0.001;
+      target.x = (1 - mouse.x) * 0.005;
       target.y = (1 - mouse.y) * 0.001;
 
-      camera.rotation.x += 0.01 * (target.y - camera.rotation.x);
-      camera.rotation.y += 0.01 * (target.x - camera.rotation.y);
+      orbit.rotation.x += 0.01 * (target.y - orbit.rotation.x);
+      orbit.rotation.y += 0.01 * (target.x - orbit.rotation.y);
     }
 
-    animate();
-    }, []);
+    // animate(); // Don't call animate here, as it should be triggered after the model is loaded
 
+  }, []);
 
-    return (
-        <canvas class="webgl"></canvas>
-    )
-
+  return <canvas className="webgl"></canvas>;
 }
 
 export default Mael;
