77ddb8ccc038b7f24f0f479ce73e4d1436420898
eymeric <eymericdechelette@gmail.com>
2023-12-08 06:28:58 +0100

    some improovment

--- front/src/Page404.jsx
+++ front/src/Page404.jsx
@@ -1,6 +1,6 @@
 import "./page404.scss";
 import "./App.scss";
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 function Page404() {
 
@@ -46,15 +46,48 @@ function Page404() {
             return newPiece;
         }
 
-
+        const checkProblems = () => {
+            for (let y = 0; y < currentPiece.length; y++) {
+                for (let x = 0; x < currentPiece[0].length; x++) {
+                    //check if the piece is out of the grid
+                    if (currentPieceCoords[1] < 0) {
+                        alert("Game over");
+                        grid = [];
+                        for (let i = 0; i < 20; i++) {
+                            grid.push([]);
+                            for (let j = 0; j < 10; j++) {
+                                grid[i].push(0);
+                            }
+                        }
+                    }
+                    if (currentPiece[y][x] > 0 && grid[Math.ceil(currentPieceCoords[1]) + y][Math.ceil(currentPieceCoords[0]) + x] > 0) {
+                        console.log("collision");
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
 
         document.addEventListener("keydown", (event) => {
             if (event.key === "ArrowLeft") {
                 currentPieceCoords[0] -= 1;
+                if (checkProblems()) {
+                    currentPieceCoords[0] += 1;
+                }
+
             } else if (event.key === "ArrowRight") {
                 currentPieceCoords[0] += 1;
+                if (checkProblems()) {
+                    currentPieceCoords[0] -= 1;
+                }
             } else if (event.key === "ArrowDown") {
-                currentPieceCoords[1] += 1;
+                if (currentPieceCoords[1] + currentPiece.length + 1 <= 20) {
+                    if (grid[Math.ceil(currentPieceCoords[1]) + currentPiece.length][Math.ceil(currentPieceCoords[0])] === 0) {
+                        currentPieceCoords[1] += 1;
+                    }
+                }
+                // currentPieceCoords[1] += 1;
             } else if (event.key === "ArrowUp") {
                 currentPiece = rotatePiece(currentPiece);
             }
@@ -66,15 +99,20 @@ function Page404() {
 
         const PIECE_SIZE = context.canvas.width / 10;
 
-        const onscreen_pieces = [];
-
         context.fillStyle = "black";
         context.fillRect(0, 0, canvas.width, canvas.height);
 
         let currentPiece = [];
         let currentPieceCoords = [0, 0];
 
-        //placed_pieces.push(currentPiece);
+        let grid = [];
+        for (let i = 0; i < 20; i++) {
+            grid.push([]);
+            for (let j = 0; j < 10; j++) {
+                grid[i].push(0);
+            }
+        }
+
 
         const generatePiece = () => {
             currentPiece = PIECES[Math.floor(Math.random() * 7)];
@@ -93,36 +131,109 @@ function Page404() {
             });
         };
 
+        const drawCase = (x, y) => {
+            context.fillRect(x * PIECE_SIZE, y * PIECE_SIZE, PIECE_SIZE, PIECE_SIZE);
+        }
+
         let tick = () => {
             context.fillStyle = "black";
             context.fillRect(0, 0, canvas.width, canvas.height);
             context.fillStyle = "white";
 
-            onscreen_pieces.forEach((piece) => {
-                drawPiece(piece[0], piece[1]);
+            grid.forEach((line) => {
+                line.forEach((value, i) => {
+                    if (value > 0) {
+                        drawCase(i, grid.indexOf(line));
+                    }
+                });
             });
 
             drawPiece(currentPiece, currentPieceCoords);
             currentPieceCoords[1] += PIECE_SIZE / 700;
-
             requestAnimationFrame(tick);
+
+            checkBottomCollision();
+            checkCollisionWithPlacedPieces();
+            checkBorderCollision();
+            checkLineCompletion();
         };
 
         requestAnimationFrame(tick);
 
+        //check colition with the bottom
+        const checkBottomCollision = () => {
+            if (currentPieceCoords[1] + currentPiece.length > 20) {
+                currentPieceCoords[1] = 20 - currentPiece.length;
+                placeFigures();
+                generatePiece();
+            }
+        };
 
+        const checkCollisionWithPlacedPieces = () => {
+            for (let y = 0; y < currentPiece.length; y++) {
+                for (let x = 0; x < currentPiece[0].length; x++) {
+                    if (currentPiece[y][x] > 0 && grid[Math.ceil(currentPieceCoords[1]) + y][Math.ceil(currentPieceCoords[0]) + x] > 0) {
+                        console.log("collision");
+                        currentPieceCoords[1] = Math.ceil(currentPieceCoords[1]) - 1;
+                        //check if the piece is out of the grid
+                        // if (currentPieceCoords[1] < 0) {
+                        //     alert("Game over");
+                        //     grid = [];
+                        //     for (let i = 0; i < 20; i++) {
+                        //         grid.push([]);
+                        //         for (let j = 0; j < 10; j++) {
+                        //             grid[i].push(0);
+                        //         }
+                        //     }
+                        // }
+                        placeFigures();
+                        generatePiece();
+                    }
+                }
+            }
+        }
+        const placeFigures = () => {
+            for (let y = 0; y < currentPiece.length; y++) {
+                for (let x = 0; x < currentPiece[0].length; x++) {
+                    if (currentPiece[y][x] > 0) {
+                        grid[Math.ceil(currentPieceCoords[1]) + y][Math.ceil(currentPieceCoords[0]) + x] = currentPiece[y][x];
+                    }
+                }
+            }
+            generatePiece();
+        }
+
+        const checkBorderCollision = () => {
+            if (currentPieceCoords[0] < 0) {
+                currentPieceCoords[0] = 0;
+            } else if (currentPieceCoords[0] + currentPiece[0].length > 10) {
+                currentPieceCoords[0] = 10 - currentPiece[0].length;
+            }
+        }
+
+        const checkLineCompletion = () => {
+            grid.forEach((line, i) => {
+                if (line.every((value) => value > 0)) {
+                    grid.splice(i, 1);
+                    grid.unshift([]);
+                    for (let j = 0; j < 10; j++) {
+                        grid[0].push(0);
+                    }
+                }
+            });
+        }
 
 
     }, []);
 
 
-return (
-    <div className="tetris">
-        <div id="grid">
-            <canvas id="tetris" width="400" height="800"></canvas>
+    return (
+        <div className="tetris">
+            <div id="grid">
+                <canvas id="tetris" width="400" height="800"></canvas>
+            </div>
         </div>
-    </div>
-);
+    );
 }
 
 export default Page404;
