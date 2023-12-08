09046748876962bd649b43e9f22dc3d85ac3d9c4
VirisOnGithub <clement.reniers@etu.univ-lyon1.fr>
2023-12-08 05:02:57 +0100

    scss little changes, blur and intensity color

--- front/src/CarteReponse.jsx
+++ front/src/CarteReponse.jsx
@@ -21,7 +21,7 @@ function CarteReponse ({ side, onNextMatch , content, logo, isInfoTrue, sourceUr
   return (
     <>
         <h1> {isCorrect ? "Vous avez donné la bonne réponse" : "Vous avez donné la mauvaise réponse"} </h1>
-        <div className="case-reponse" style={{backgroundColor: isCorrect ? "rgba(0,255,0,0.1)" : "rgba(255,0,0,0.1)"}}>
+        <div className="case-reponse" style={{backgroundColor: isCorrect ? "rgba(0,255,0,0.3)" : "rgba(255,0,0,0.3)"}}>
             <img src={logo} className="logo" alt="Vite logo" />
             <p className = "text-reponse" > {content} <a href={SourceUrl}>Source</a></p>
         </div>
