2b9a3a11f7382730dee3ba61b0bafcc7af9c8288
tom <tom@tom.com>
2023-12-08 05:01:23 +0100

    css for explicatio of tinder

--- front/src/Tinder.jsx
+++ front/src/Tinder.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import TinderCard from "./TinderCard";
 import CarteReponse from "./CarteReponse";
+import "./App.scss";
 
 const Tinder = () => {
     const [isResultPage, setIsResultPage] = useState(false);
@@ -69,11 +70,14 @@ const Tinder = () => {
 
     return (
         <>
+        <div className="TinderText">
+
             <h1> Truth Quest for the Earth  </h1>
             <p className="gras"> Bienvenue dans TruthQuest, le jeu qui met vos compétences de détection des fausses informations à l'épreuve ! 🕵️‍♂️🔍 <br/> <br/> </p>
             <p className="gras">1. Découvrez la Vérité : </p> <p> Vous êtes un détective de la vérité, chargé de trier les informations authentiques des fausses. Chaque niveau présente une série d'explications sur le changement climatique. <br/> </p> 
             <p className="gras">2. Choisissez le Mensonge :</p> <p> Lisez attentivement chaque explication et identifiez la fausse information parmi les propositions. Faites glisser votre choix vers le bon côté de l'écran pour révéler si vous avez trouvé la réponse correcte. <br/> </p> 
             <p className="gras">3. Points et Niveaux : </p> <p>Gagnez des points en trouvant les fausses informations. Plus vous en découvrez, plus vous montez de niveau. <br/> </p>
+            </div>
             <div className="tinder-page">
                 {
                     !isResultPage && <TinderCard save={handleSubmit} logo={CardLogo} content={CardContent} />
@@ -83,6 +87,7 @@ const Tinder = () => {
                 }
             </div>
         </>
+        
     )
 }
 
