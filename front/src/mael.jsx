1ebcf161a4a8c6b03972a99d4f7be61bfcccf3a0
Xeon0X <xeon0x.gaming@gmail.com>
2023-12-08 02:07:17 +0100

    Rotation movement based on mouse position

--- front/src/mael.jsx
+++ front/src/mael.jsx
@@ -12,7 +12,6 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
 function Mael(){
-   
 
 useEffect(() => {
     const width = window.innerWidth;
@@ -40,30 +39,59 @@ useEffect(() => {
     
     //Mesh 2
     const geometry2 = new THREE.SphereGeometry(1, 20);
-    const material2 = new THREE.MeshBasicMaterial({color: 0xfff000, wireframe: true});
+    const material2 = new THREE.MeshBasicMaterial({color: 0xfff0f0, wireframe: true});
     const sphere = new THREE.Mesh(geometry2, material2)
     sphere.position.x = 0;
     scene.add(sphere);
     
+    //Import models
+    let earthModel;
+    const loader = new GLTFLoader();
+
+    loader.load( '../public/the_earth.gltf', function ( gltf ) {
+        earthModel = gltf.scene;
+        earthModel.scale.set(1,1,1);
+        scene.add(gltf.scene);
+    }, undefined, function ( error ) {
+        console.error( error );
+    } );
+
     //Orbit 2
     const moonOrbit = new THREE.Group();
     moonOrbit.add(sphere);
     scene.add(moonOrbit);
     sphere.position.set(0, 0, 8);
+
+    const target = new THREE.Vector2();
+    const windowHalf = new THREE.Vector2(window.innerWidth / 2, window.innerHeight / 2);
+    document.addEventListener('mousemove', onMouseMove, false);
+    const mouse = new THREE.Vector2();
+    
+    function onMouseMove(event) {
+        mouse.x = (event.clientX - windowHalf.x)
+        mouse.y = (event.clientY - windowHalf.y)
+    }
     
     function animate() {
       requestAnimationFrame(animate);
       renderer.render(scene, camera);
+      sphere.rotation.x += 0.01;
+
+      target.x = (1 - mouse.x) * 0.001;
+      target.y = (1 - mouse.y) * 0.001;
+
+      camera.rotation.x += 0.01 * (target.y - camera.rotation.x);
+      camera.rotation.y += 0.01 * (target.x - camera.rotation.y);
     }
+
     animate();
-}, []);
+    }, []);
 
 
-return (
-    <canvas class="webgl"></canvas>
-)
+    return (
+        <canvas class="webgl"></canvas>
+    )
+
 }
-
-
 
 export default Mael;
